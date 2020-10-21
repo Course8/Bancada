6b07c41e9bd6e20131dc7b0e6f1c0c2ecb4ee7de
Course <superleogame@gmail.com>
2020-10-20 22:12:57 -0300

    Mais mudanças.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import GitHub from './pages/github/github.js'
 import Footer from './pages/footer/footer.js'
+import Apidragon from './pages/api-dragon/apidragon'
 
 function App() {
   return (
--- src/pages/github/github.js
+++ src/pages/github/github.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './github.css';
 
 class GitHub extends Component{
     constructor(){
@@ -7,14 +8,30 @@ class GitHub extends Component{
             user: [],
             repo: [],
         }
+        this.input = React.createRef()
+        this.mudarUsuario = this.mudarUsuario.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount(){
-        fetch('https://api.github.com/users/Course8')              //Esse comando pega as infos da API github
-        .then( response => response.json() )                       //Esse comando converte as infos para o formato JSON 
-        .then( data => {this.setState({user: data})} )             //Esse comando coloca o valor 'data2' dentro do atributo 'data'
+        
+    }
 
-        fetch('https://api.github.com/users/Course8/repos')        //Esse comando pega as infos da API github dos repositórios
+    handleChange(event){
+        this.setState({value: event.target.value})
+    }
+
+    mudarUsuario(event){
+        this.setState({value: event.target.value})
+        let user = this.state.value;
+        let url = "https://api.github.com/users/" + user;
+        let url_repo = url + "/repos"
+
+        fetch(url)                                                 //Esse comando pega as infos da API github
+        .then( response => response.json() )                       //Esse comando converte as infos para o formato JSON
+        .then( data => {this.setState({user: data})} );            //Esse comando coloca o valor 'data2' dentro do atributo 'data'
+
+        fetch(url_repo)                                            //Esse comando pega as infos da API github dos repositórios
         .then( response => response.json() )                       //Esse comando converte as infos para o formato JSON 
         .then( data => {this.setState({repo: data})} )             //Esse comando coloca o valor 'data2' dentro do atributo 'data'
     }
@@ -22,26 +39,46 @@ class GitHub extends Component{
     render(){
         const {user, repo} = this.state;
         return<>
-          <div className="header titulo col py-4">{user.login}</div>
+          <div className="header col">Portal Course</div>
+          <br></br>
+          <div className="fundoMain container">
+            <div className="textos">Insira aqui um nome de usuário do GitHub:</div>
+            <input id="input" className="bg-card mt-1" ref={this.input} onChange={this.handleChange}></input> <button onClick={this.mudarUsuario} className="bg-card">Buscar</button>
+          </div>
+          <br></br>
+          <div className="container fundoMain">
+              <div className="row">
+                    <div className="col-3">
+                        <p className="info py-4 card bg-card">Infos do Usuário</p>
+                        <div>
+                            <img className="circle-photo" src={user.avatar_url}></img>
+                        </div>
 
-          <div className="container">
-                <div>
-                    <p className="info py-4">Infos do Usuário do GitHub</p>
-                </div>
+                        <div className="titulo py-4">
+                            {user.login}    
+                        </div>
 
-                <div className="row">
-                    <div className="col">
+                        <div className="info card bg-card py-4">
+                            <p>Followers: {user.followers}</p>
+                            <p>Following: {user.following}</p>
+                        </div>
+                    </div>
+
+                    <div className="col-9">
+                        <p className="info py-4 card bg-card mb-4">Repositórios</p>
+
                         {repo.map(
-                            repositorio => 
-                            <p className="textos">
+                            repositorio =>
+                            <div className="textos card bg-card">
                                 Nome do Repositório: {repositorio.name} <br></br>
                                 Descrição: {repositorio.description} <br></br>
                                 Link do Repositório: <a href={repositorio.html_url}>Clique Aqui</a>
-                            </p>
+                            </div>
                         )}
                     </div>
               </div>
           </div>
+          <br></br>
         </>
     }
 }
